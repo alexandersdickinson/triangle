f2b515aa8a6d5ecc803150058c4f1e9003d1854a
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-09-08 22:41:57 -0700

    First specs passed.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,2 +1,37 @@
 class Triangle
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+  
+  def isosceles?
+    if @sides.uniq().length() > 2
+      false
+    else
+      true
+    end
+  end
+  
+  def scalene?
+    if self.isosceles?() == false
+      true
+    else
+      false
+    end
+  end
+  
+  def equilateral?
+    if @sides.uniq().length() > 1
+      false
+    else
+      true
+    end
+  end
+  
+  def triangle?
+    if @sides[0] > @sides[1] + @sides[2] || @sides[1] > @sides[0] + @sides[2] || @sides[2] > @sides[0] + @sides[1]
+      false
+    else
+      true
+    end
+  end
 end
